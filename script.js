b6b3df893bfc96b2db45f58d60e02a61d90c087a
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2025-01-22 03:09:04 +0000

    Update script.js

--- script.js
+++ script.js
@@ -4,16 +4,7 @@ function sendMail(){
         email : document.getElementById('email').value,
         message : document.getElementById('message').value,
     }   
-    emailjs.send('service_iyooo0p', 'template_4wi67kr', params)
-    .then(
-        res => {
-            document.getElementById('name').value = ''
-            document.getElementById('email').value = ''
-            document.getElementById('message').value = ''
-        }
-    )
-
-    (alert('Email Enviado com Sucesso!'))
+    emailjs.send('service_iyooo0p', 'template_4wi67kr', params).then(alert('Email Enviado com Sucesso!'))
     
 }
 
