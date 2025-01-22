26b3140e83cf136f5c1d88872cd8baee94456096
tiagocodinha <98787370+tiagocodinha@users.noreply.github.com>
2025-01-22 02:23:40 +0000

    Mail

---
+++ script.js
@@ -0,0 +1,9 @@
+function sendMail(){
+    let params = {
+        name : document.getElementById('name').value,
+        email : document.getElementById('email').value,
+        message : document.getElementById('message').value,
+    }   
+    emailjs.send('service_iyooo0p', 'template_4wi67kr', params).then(alert('Email Sent!'))
+    
+}
